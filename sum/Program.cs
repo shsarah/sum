179e25a19ca1d46b771fcdeaf5245a1c1e819cc2
shsarah <saraash@code.edu.az>
2022-12-06 18:18:18 +0400

    sum

---
+++ sum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace sum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Birinci ededi daxil edin:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ikinci ededi daxil edin:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int cem = a + b;
+        Console.WriteLine("Cem:" + cem);
+    }
+}
+
